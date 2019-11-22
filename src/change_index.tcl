0e940acdb76edf7a53cd881859c13d5e5b008b1c
stef_sport_2002 <stef_sport_2002@b88a8de6-53bf-4a58-bc52-7dee9acc7ef3>
2019-11-22 02:58:30 +0000

    added rawtovcd in build system, added copyright notices.

--- src/change_index.tcl
+++ src/change_index.tcl
@@ -1,5 +1,28 @@
+#
+#  File: change_index.tcl
+#  
+#  This file is part of XSCHEM,
+#  a schematic capture and Spice/Vhdl/Verilog netlisting tool for circuit 
+#  simulation.
+#  Copyright (C) 1998-2019 Stefan Frederik Schippers
+# 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+# 
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
 
 # increments index of bussed label by $incr
+# works if only one net label/pin  is selected
 proc change_index {incr} {
 
   set sel [xschem selected_set]
@@ -8,7 +31,7 @@ proc change_index {incr} {
     regsub {.*\[} $mylabel {} myindex
     regsub {\].*} $myindex {} myindex
     regsub {\[.*} $mylabel {} mybasename
-    if { [regexp {^[0-9][0-9]*$} $myindex] } {
+    if { [regexp {^[1-9][0-9]*$} $myindex] } {
       set myindex [expr $myindex + $incr]
       set mylabel "$mybasename\[$myindex\]"
       xschem setprop instance $i lab $mylabel
